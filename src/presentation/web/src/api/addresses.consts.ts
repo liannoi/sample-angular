3340621ed997163199f51cc18a9128d4da0092c5
Maksym Liannoi <liannoi444@gmail.com>
2020-05-21 07:38:03 +0300

    Added component for filter search products

--- src/presentation/web/src/api/addresses.consts.ts
+++ src/presentation/web/src/api/addresses.consts.ts
@@ -1,5 +1,5 @@
-export const baseApiAddress: string = 'https://localhost:5001/api';
-// export const baseApiAddress: string = 'http://liannoi01-001-site1.htempurl.com/api';
+// export const baseApiAddress: string = 'https://localhost:5001/api';
+export const baseApiAddress: string = 'http://liannoi01-001-site1.htempurl.com/api';
 
 export const manufacturersEndpoint: string = `${baseApiAddress}/manufacturers`;
 export const productsEndpoint: string = `${baseApiAddress}/products`;
---
+++ src/presentation/web/src/app/components/storage/products/get/filter/product-get-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-get-filter',
+  templateUrl: './product-get-filter.component.html',
+  styleUrls: ['./product-get-filter.component.css']
+})
+export class ProductGetFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
