3fa6b44f00e61f760571262844df69815d3092c5
Maksym Liannoi <liannoi444@gmail.com>
2020-05-24 20:35:52 +0300

    Connection address changed

--- src/presentation/web/src/app/shared/addresses.consts.ts
+++ src/presentation/web/src/app/shared/addresses.consts.ts
@@ -1,8 +1,8 @@
-export const serverAddress: string = 'https://localhost:5001';
-export const baseApiAddress = `${serverAddress}/api`;
+// export const serverAddress: string = 'https://localhost:5001';
+// export const baseApiAddress = `${serverAddress}/api`;
 
-// export const serverAddress:string= 'http://liannoi01-001-site1.htempurl.com';
-// export const baseApiAddress: string = `${serverAddress}/api`;
+export const serverAddress:string= 'http://liannoi01-001-site1.htempurl.com';
+export const baseApiAddress: string = `${serverAddress}/api`;
 
 export const manufacturersEndpoint = `${baseApiAddress}/manufacturers`;
 export const productsEndpoint = `${baseApiAddress}/products`;
